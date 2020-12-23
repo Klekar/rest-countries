133e1cc8cc9290ea5d35aae0c8abc959e3c4e77c
Karel Klecka <kklecka@seznam.cz>
2020-12-23 11:04:09 +0100

    Tweak list styles

---
+++ src/script/country_detail.js
@@ -0,0 +1,15 @@
+class CountryDetail extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+  
+    render() {
+        return (
+            <div id="country-detail">
+                <div id="detail-controls">
+                    <button>← Back</button>
+                </div>
+            </div>
+        );
+    }
+}
